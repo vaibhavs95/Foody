3983a96002359afa713032a25a10b0de495bd376
vaibhavs95 <vaibhavsingh911@gmail.com>
2018-05-18 19:31:26 +0530

    initial commit

---
+++ Foody/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Vaibhav Singh on 18/05/18.
+//  Copyright © 2018 Vaibhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
