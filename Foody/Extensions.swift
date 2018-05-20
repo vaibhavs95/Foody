12f658682bf89a5d8e4289778de00254e8682d3b
vaibhavs95 <vaibhavsingh911@gmail.com>
2018-05-20 14:46:31 +0530

    added bicolour extension to initialise from hex string

--- Foody/Extensions.swift
+++ Foody/Extensions.swift
@@ -11,6 +11,28 @@ import UIKit
 
 extension UIColor {
 
+    public convenience init?(hexString: String) {
+        let r, g, b, a: CGFloat
+        let hexColor = "ff\(hexString)"
+
+        if hexColor.count == 8 {
+            let scanner = Scanner(string: hexColor)
+            var hexNumber: UInt64 = 0
+
+            if scanner.scanHexInt64(&hexNumber) {
+                r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                self.init(red: r, green: g, blue: b, alpha: a)
+                return
+            }
+        }
+
+        return nil
+    }
+
     static let tealBlue = UIColor(red: 53/255, green: 92/255, blue: 125/255, alpha: 1)
     static let defaultBlue = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
 }
